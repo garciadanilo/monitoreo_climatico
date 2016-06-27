30d870e8bd730a5189f82e790929580a7008720e
Danilo Antonio Garcia Sales <danilogarcia024@gmail.com>
2016-06-27 16:54:45 -0500

    Agregado foundation manualmente, todavía problema con el menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,11 +2,13 @@
 //= require jquery_ujs
 //= require foundation
 //= require responsive-tables
+//= what-input
 
+/*
 $(document).foundation({
   topbar: {
       custom_back_text: true,
       back_text: 'Volver'
   }
-});
+});*/
 $(function(){ $(document).foundation(); });
